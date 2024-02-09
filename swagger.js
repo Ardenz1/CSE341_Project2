f758aab9a3e888eae7be883867bc32ba8bb300bb
arden <ardenbyui.edu>
2024-02-09 10:46:48 -0700

    minor changes

--- swagger.js
+++ swagger.js
@@ -1,16 +1,16 @@
-const swaggerAutogen = require('swagger-autogen')();
+const swaggerAutogen = require("swagger-autogen")();
 
 const doc = {
   info: {
-    title: 'My API',
-    description: 'Recipe'
+    title: "My API",
+    description: "Recipe",
   },
-  host: 'localhost:8080',
-  schemes: ['http']
+  host: "cse-341project2.onrender.com",
+  schemes: ["https"],
 };
 
-const outputFile = './swagger.json';
-const endpointsFiles = ['./routes/index.js'];
+const outputFile = "./swagger.json";
+const endpointsFiles = ["./routes/index.js"];
 
 // generate swagger.json
 swaggerAutogen(outputFile, endpointsFiles, doc);
