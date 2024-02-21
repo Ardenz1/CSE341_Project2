e51e62fd4556b3130d2db30e6c136e8de54ac310
arden <ardenbyui.edu>
2024-02-21 16:46:51 -0700

    personalW7v1

--- routes/recipeRoutes.js
+++ routes/recipeRoutes.js
@@ -1,24 +1,26 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const recipeController = require('../controllers/recipeController');
-const validation = require('../middleware/validate');
+const recipeController = require("../controllers/recipeController");
+const validation = require("../middleware/validate");
+const { requiresAuth } = require("express-openid-connect");
+const Recipe = require("../models/recipe");
 
 
 // GET all recipes
-router.get('/', recipeController.getAll);
+router.get("/",  requiresAuth(), recipeController.getAll);
 
 // router.get('/recipes/:chefId', recipeController.getRecipesByChefId);
 
 // GET a recipe by ID
-router.get('/:id', recipeController.getRecipeById);
+router.get("/:id", recipeController.getRecipeById);
 
 // POST a new recipe
-router.post('/', validation.saveRecipe, recipeController.createRecipe);
+router.post("/", validation.saveRecipe, recipeController.createRecipe);
 
 // PUT update a recipe
-router.put('/:id', validation.saveRecipe, recipeController.updateRecipe);
+router.put("/:id", validation.saveRecipe, recipeController.updateRecipe);
 
 // DELETE a recipe
-router.delete('/:id', recipeController.deleteRecipe);
+router.delete("/:id", recipeController.deleteRecipe);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,9 +1,39 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongodb = require("./db/db.config");
+const recipesRouter = require("./routes/recipeRoutes");
+
+
+// authentication
+const { auth, requiresAuth } = require("express-openid-connect");
 
 const port = process.env.PORT || 8080;
 const app = express();
+
+// authentication
+const config = {
+  authRequired: false,
+  auth0Logout: true,
+  secret: process.env.SECRET,
+  baseURL: process.env.BASE_URL,
+  clientID: process.env.CLIENT_ID,
+  issuerBaseURL: process.env.ISSUER_BASE_URL,
+};
+
+// auth router attaches /login, /logout, and /callback routes to the baseURL
+app.use(auth(config));
+
+// req.isAuthenticated is provided from the auth router
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out");
+});
+
+app.get("/profile", requiresAuth(), (req, res) => {
+  res.send(JSON.stringify(req.oidc.user));
+});
+
+// get the recipes
+app.use("/recipes", recipesRouter);
 
 app
   .use(bodyParser.json())
@@ -13,10 +43,12 @@ app
   })
   .use("/", require("./routes"));
 
-  process.on('uncaughtException', (err, origin) => {
-    console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
-  });
-  
+process.on("uncaughtException", (err, origin) => {
+  console.log(
+    process.stderr.fd,
+    `Caught exception: ${err}\n` + `Exception origin: ${origin}`
+  );
+});
 
 mongodb.initDb((err) => {
   if (err) {
